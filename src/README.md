2c2cb885032d28679bbb3ff4346e18b5715219a7
coel16 <modopojken@gmail.com>
2019-03-29 13:15:06 +0100

    Skapat gui form och justerat så att den har alla funktioner som jag behöver för att skapa programmet.

---
+++ src/README.md
@@ -0,0 +1,3 @@
+                                           --Slutprojekt i Programmering 2--
+
+29-03-2019: Skapat mitt GUI interface.
---
+++ src/hejsansvejsan.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class hejsansvejsan {
+    private JButton exitButton;
+    private JTextField skrivDinGissningHärTextField;
+    private JTextField gissaOrdetTextField;
+    private JTextArea härKommerLedtrådarnaTextArea;
+    private JTextArea spelareSomÄrMedTextArea;
+    private JTextField antalPoängSomGesTextField;
+    private JTextField dinaPoängTextField;
+    private JTextField antalLedtrådarUteTextField;
+    private JTextField rättEllerFelIndikatorTextField;
+}
