f482ca8932e5729e7d5ac7d097393bb801c1cb09
coel16 <modopojken@gmail.com>
2019-04-05 13:33:10 +0200

    Skapat gui form och justerat så att den har alla funktioner som jag behöver för att skapa programmet.
    Har nu också lagt till exit knapp.

--- src/hejsansvejsan.java
+++ src/hejsansvejsan.java
@@ -12,21 +12,80 @@ public class hejsansvejsan {
     private JTextField dinaPoängTextField;
     private JTextField antalLedtrådarUteTextField;
     private JTextField rättFelTextField;
-    private JPanel Panelen;
+    private JPanel fuskknapp;
+    private JTextField skrivDinGissningNedanförTextField;
+    private JTextField ledareTextField;
+    private JRadioButton fuskRadioButton;
+    private JTextField rättsvar;
+
+    String rättord = "tre";
+    int score = 0;
+    int AntalPoängSomGes = 5;
+    int rättirad;
+    int felirad;
+    JFrame frame;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("hejsansvejsan");
-        frame.setContentPane(new hejsansvejsan().Panelen);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        hejsansvejsan hs = new hejsansvejsan();
     }
 
     public hejsansvejsan() {
+        System.out.println("starting");
+        frame = new JFrame("hejsansvejsan");
+        frame.setContentPane(this.fuskknapp);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
+            }
+        });
+        skrivDinGissningHärTextField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String Guess = skrivDinGissningHärTextField.getText();
+                if(Guess.equals(rättord)){
+                    score ++;
+                    String svar = Integer.toString(score);
+                    dinaPoängTextField.setText(svar);
+                    System.out.println("rätt");
+                    skrivDinGissningHärTextField.setText("");
+                    felirad = 0;
+                    rättirad++;
+                    rättFelTextField.setText("Rätt! " + "Du har haft rätt " + rättirad + " gånger i rad!");
+                    AntalPoängSomGes = 5;
+                }else{
+                    AntalPoängSomGes--;
+                    felirad++;
+                    String FelSvarPoäng = Integer.toString(AntalPoängSomGes);
+                    antalPoängSomGesTextField.setText(FelSvarPoäng);
+                    skrivDinGissningHärTextField.setText("");
+                    rättFelTextField.setText("Fel! " + "Du har haft fel " + felirad + " gånger i rad!");
+                    if (AntalPoängSomGes <= 0){
+
+                        JOptionPane.showMessageDialog(null, "Du gissade fel för många gånger i rad.");
+                        System.out.println("Exiting");
+                        System.exit(1);
+                    }
+
+                }
+
+            }
+        });
+        dinaPoängTextField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int yourscore = 1;
+
+            }
+        });
+        fuskRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //rättsvar
             }
         });
     }
